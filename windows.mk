36e172c33aa590d010e71f38664f002a29167705
ahaooahaz <ahaooahaz@outlook.com>
2025-04-11 14:25:19 +0800

    [feat]: makefile in linux and windows

---
+++ windows.mk
@@ -0,0 +1,12 @@
+SHELL = cmd.exe
+TARGETS = wezterm nushell
+
+env: $(TARGETS)
+
+wezterm:
+	powershell -Command \
+	'robocopy "configs\\wezterm\\" "%USERPROFILE%\\.config\\wezterm\\" /MIR /IS /R:0 /W:0 /V ; $$true'
+
+nushell:
+	powershell -Command \
+	'robocopy "configs\\nushell\\" "%USERPROFILE%\\AppData\\Roaming\\nushell\\" /MIR /IS /R:0 /W:0 /V ; $$true'
